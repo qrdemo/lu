c9179fd1cc8898afd975495e6ca0fa593a275302
xuezu <ruoqi.drq@alibaba-inc.com>
2014-03-06 13:24:01 +0800

    add restart logic

--- routes/screen.js
+++ routes/screen.js
@@ -1,6 +1,9 @@
-var env = require('../lib/env')
+var env = require('../lib/env');
 
 // GET
 exports.main = function(req, res){
+  var app = req.app;
+  if ('127.0.0.1' == req.ip || env.ip == req.ip) app.emit('game restart');
+
   res.render('screen/main', env);
 };
